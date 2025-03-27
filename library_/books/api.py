39690e02e581e8d9b9a286de1805a380b65366fb
ninimefarishvili <nmefarishvili3@gmail.com>
2025-03-27 12:34:44 -0700

    dzlivs

--- library_/books/api.py
+++ library_/books/api.py
@@ -9,6 +9,11 @@ def get_books(request):
     books = Book.objects.all()  
     return [BookSchema.from_orm(book) for book in books]  
 
+@router.get("/borrowedbooks")
+def borrowed_books(request):
+    borrowed_books = books 
+
+
 @router.get("/Genre")
 def get_genres(request):
     genres = Genre.objects.all()  
--- library_/books/models.py
+++ library_/books/models.py
@@ -1,42 +1,27 @@
-from django.db import models
-from django.utils import timezone
+from ninja import Router
+from .models import Book, Genre
+from .serializers import BookSchema, GenreSchema
 
+router = Router()
 
-class Book(models.Model):
-    title = models.CharField("სათაური", max_length=18, null=False, blank=False)
-    author = models.CharField("ავტორი", max_length=18, null=False, blank=False)
-    published_date = models.DateField()
-    genres = models.ManyToManyField("Genre")  
+@router.get("/books")
+def get_books(request):
+    books = Book.objects.all()  
+    return [BookSchema.from_orm(book) for book in books]  
+
+@router.get("/borrowedbooks")
+def borrowed_books(request):
     
-    borrowed_by = models.CharField("მომხმარებელი", max_length=100, blank=True, null=True)  
-    borrow_date = models.DateTimeField("აღების თარიღი", blank=True, null=True)  
+    borrowed_books = Book.objects.filter(borrowed_by__isnull=False) 
+    return [BookSchema.from_orm(book) for book in borrowed_books]
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+@router.get("/availablebooks")
+def available_books(request):
     
-    def is_borrowed(self):
-        return bool(self.borrowed_by)
-    
-    def borrow(self, borrower_name):
-        if not self.is_borrowed():
-            self.borrowed_by = borrower_name
-            self.borrow_date = timezone.now()
-            self.save()
-            return True
-        return False
-    
-    def return_book(self):
-        self.borrowed_by = None
-        self.borrow_date = None
-        self.save()
-        return True
+    available_books = Book.objects.filter(borrowed_by__isnull=True) 
+    return [BookSchema.from_orm(book) for book in available_books]
 
-    def __str__(self):
-        return self.title
-
-class Genre(models.Model):  
-    name = models.CharField("ჟანრი", max_length=50, null=False, blank=False)
-    description = models.TextField("აღწერა", blank=True, null=True)  
-
-    def __str__(self):
-        return self.name
+@router.get("/genres")
+def get_genres(request):
+    genres = Genre.objects.all()  
+    return [GenreSchema.from_orm(genre) for genre in genres]
