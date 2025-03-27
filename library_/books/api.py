5dd0ee9545d2158be49313d8e47e548823932c29
ninimefarishvili <164371339+ninimefarishvili@users.noreply.github.com>
2025-03-27 19:29:38 +0400

    Update api.py

--- library_/books/api.py
+++ library_/books/api.py
@@ -8,3 +8,8 @@ router = Router()
 def get_books(request):
     books = Book.objects.all()  
     return [BookSchema.from_orm(book) for book in books]  
+
+@router.get("/Genre")
+def get_genres(request):
+    genres = Genre.objects.all()  
+    return [GenreSchema.from_orm(genre) for genre in genres]  
